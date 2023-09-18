8038a17758484e4ac18bca59d514cde76fff6137
ard-hmd <ec2-user@ip-172-31-36-140.eu-west-3.compute.internal>
2023-09-18 11:44:47 +0000

    added local vars

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_eks_node_group" "node-ec2" {
 
 resource "aws_eks_addon" "addons" {
   for_each          = { for addon in var.addons : addon.name => addon }
-  cluster_name      = aws_eks_cluster.eks-cluster.id
+  cluster_name      = var.eks_cluster_name
   addon_name        = each.value.name
   addon_version     = each.value.version
   resolve_conflicts = "OVERWRITE"
