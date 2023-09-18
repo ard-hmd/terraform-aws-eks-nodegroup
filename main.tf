510212376c80e8c797f2cce4572e7510b3cae8fc
ard-hmd <ec2-user@ip-172-31-36-140.eu-west-3.compute.internal>
2023-09-18 11:25:17 +0000

    added module

---
+++ main.tf
@@ -0,0 +1,85 @@
+module "aws_vpc" {
+  source                  = "github.com/ard-hmd/terraform-aws-vpc"
+  vpc_cidr                = var.vpc_cidr
+  environment             = var.environment
+  azs                     = var.azs
+  public_subnets_cidr     = var.public_subnets_cidr
+  private_subnets_cidr    = var.private_subnets_cidr
+}
+
+resource "aws_iam_role" "NodeGroupRole" {
+  name = "EKSNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+//This policy allows Amazon EKS worker nodes to connect to Amazon EKS Clusters.
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+// Provides read-only access to Amazon EC2 Container Registry repositories.
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+// This policy provides the Amazon VPC CNI Plugin (amazon-vpc-cni-k8s) the permissions it requires to modify the IP address configuration on your EKS worker nodes. This permission set allows the CNI to list, describe, and modify Elastic Network Interfaces on your behalf
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+resource "aws_eks_node_group" "node-ec2" {
+  for_each        = { for node_group in var.node_groups : node_group.name => node_group }
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = each.value.name
+  node_role_arn   = aws_iam_role.NodeGroupRole.arn
+  subnet_ids      = flatten(module.aws_vpc.private_subnets_ids)
+
+  scaling_config {
+    desired_size = try(each.value.scaling_config.desired_size, 2)
+    max_size     = try(each.value.scaling_config.max_size, 3)
+    min_size     = try(each.value.scaling_config.min_size, 1)
+  }
+
+  update_config {
+    max_unavailable = try(each.value.update_config.max_unavailable, 1)
+  }
+
+  ami_type       = each.value.ami_type
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
+  disk_size      = each.value.disk_size
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
+}
+
+resource "aws_eks_addon" "addons" {
+  for_each          = { for addon in var.addons : addon.name => addon }
+  cluster_name      = aws_eks_cluster.eks-cluster.id
+  addon_name        = each.value.name
+  addon_version     = each.value.version
+  resolve_conflicts = "OVERWRITE"
+}
+
+resource "aws_iam_openid_connect_provider" "default" {
+  url             = "https://${local.oidc}"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+}
---
+++ vars.tf
@@ -0,0 +1,62 @@
+# Set the default AWS region for the resources
+variable "aws_region" {
+  default = "eu-west-3"
+  description = "Default AWS region where resources will be created."
+}
+
+# Define the availability zones for the resources
+variable "azs" {
+  default = ["eu-west-3a", "eu-west-3b"]
+  description = "List of availability zones within the specified AWS region."
+}
+
+# Set the default environment label for the resources
+variable "environment" {
+  default = "prod"
+  description = "Environment label to be used for naming and tagging resources."
+}
+
+# Set the CIDR block for the VPC
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "CIDR block for the Virtual Private Cloud (VPC)."
+}
+
+# Define the CIDR blocks for public subnets
+variable "public_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.0.0/20", "10.0.128.0/20"]
+  description = "List of CIDR blocks for the public subnets within the VPC."
+}
+
+# Define the CIDR blocks for private subnets
+variable "private_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.16.0/20", "10.0.144.0/20"]
+  description = "List of CIDR blocks for the private subnets within the VPC."
+}
+
+variable "addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
+  default = [
+    {
+      name    = "kube-proxy"
+      version = "v1.27.4-eksbuild.2"
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.15.0-eksbuild.2"
+    },
+    {
+      name    = "coredns"
+      version = "v1.10.1-eksbuild.4"
+    },
+    {
+      name    = "aws-ebs-csi-driver"
+      version = "v1.22.0-eksbuild.2"
+    }
+  ]
+}
