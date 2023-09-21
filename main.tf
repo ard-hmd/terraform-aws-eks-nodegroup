eca29673dc6f67d581cf80a63574e37f3f5a7c5e
ard-hmd <138102964+ard-hmd@users.noreply.github.com>
2023-09-21 09:27:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Create an IAM role for EKS node group with a specific assume role policy.
 resource "aws_iam_role" "NodeGroupRole" {
   name = "EKSNodeGroupRole"
   assume_role_policy = jsonencode({
@@ -12,31 +13,36 @@ resource "aws_iam_role" "NodeGroupRole" {
       },
     ]
   })
+  tags = {
+    Name = "${var.resource_name_prefix}iam-role"
+  }
 }
 
+# Attach the Amazon EBS CSI driver policy to the IAM role.
 resource "aws_iam_role_policy_attachment" "ebs_csi_controller_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.NodeGroupRole.name
 }
 
-//This policy allows Amazon EKS worker nodes to connect to Amazon EKS Clusters.
+# Attach the Amazon EKS worker node policy to the IAM role.
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.NodeGroupRole.name
 }
 
-// Provides read-only access to Amazon EC2 Container Registry repositories.
+# Attach the Amazon EC2 Container Registry ReadOnly policy to the IAM role.
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.NodeGroupRole.name
 }
 
-// This policy provides the Amazon VPC CNI Plugin (amazon-vpc-cni-k8s) the permissions it requires to modify the IP address configuration on your EKS worker nodes. This permission set allows the CNI to list, describe, and modify Elastic Network Interfaces on your behalf
+# Attach the Amazon EKS CNI policy to the IAM role.
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.NodeGroupRole.name
 }
 
+# Create an EKS node group.
 resource "aws_eks_node_group" "node-ec2" {
   for_each        = { for node_group in var.node_groups : node_group.name => node_group }
   cluster_name    = var.eks_cluster_name
@@ -45,9 +51,9 @@ resource "aws_eks_node_group" "node-ec2" {
   subnet_ids      = flatten([var.public_subnets_ids, var.private_subnets_ids])
 
   scaling_config {
-    desired_size = try(each.value.scaling_config.desired_size, 2)
-    max_size     = try(each.value.scaling_config.max_size, 3)
-    min_size     = try(each.value.scaling_config.min_size, 1)
+    desired_size = try(each.value.scaling_config.desired_size, var.default_scaling_config.desired_size)
+    max_size     = try(each.value.scaling_config.max_size, var.default_scaling_config.max_size)
+    min_size     = try(each.value.scaling_config.min_size, var.default_scaling_config.min_size)
   }
 
   update_config {
@@ -65,20 +71,28 @@ resource "aws_eks_node_group" "node-ec2" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
   ]
   tags = {
-    Name = "MyWorkerInstanceName"
+    Name = "${var.resource_name_prefix}${each.value.name}"
   }
 }
 
+# Create EKS addons.
 resource "aws_eks_addon" "addons" {
   for_each          = { for addon in var.addons : addon.name => addon }
   cluster_name      = var.eks_cluster_name
   addon_name        = each.value.name
   addon_version     = each.value.version
   resolve_conflicts = "OVERWRITE"
+  tags = {
+    Name = "${var.resource_name_prefix}addon-${each.value.name}"
+  }
 }
 
+# Create an OpenID Connect provider.
 resource "aws_iam_openid_connect_provider" "default" {
   url             = "https://${local.oidc}"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+  tags = {
+    Name = "${var.resource_name_prefix}openid-connect-provider"
+  }
 }
